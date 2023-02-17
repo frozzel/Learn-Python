76db142632d880194d7039f53d523600aeb31e12
Dennis H <111554091+frozzel@users.noreply.github.com>
2023-02-16 23:40:23 -0500

    day five practice

---
+++ for_loop.py
@@ -0,0 +1,6 @@
+fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits:# This will loop through the list of fruits
+  print(fruit)# This will print each fruit in the list
+  print(fruit + " Pie")# This will print each fruit in the list with the word pie after it
+print(fruits) # This will print the list of fruits, it is not part of the for loop

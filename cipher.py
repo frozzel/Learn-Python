24b73efc0661bb1e5f366196b1d901620efb1f82
Dennis H <111554091+frozzel@users.noreply.github.com>
2023-02-18 12:07:42 -0500

    cipher finished

--- cipher.py
+++ cipher.py
@@ -1,3 +1,4 @@
+from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
@@ -5,18 +6,26 @@ def caesar(start_text, shift_amount, cipher_direction):
   if cipher_direction == "decode":
     shift_amount *= -1
   for char in start_text:
-    position = alphabet.index(char)
-    new_position = position + shift_amount
-    end_text += alphabet[new_position]
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
     
   print(f"Here's the {cipher_direction}d result: {end_text}")
 
-
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-
-
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+print(logo)
+should_cont = True
+while should_cont:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  
+  if shift > 26:
+    shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  run = input("Star Again Y/N  ").lower()
+  if not run == 'y':
+    should_cont = False
+    print("Good Bye!")

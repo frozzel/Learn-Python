04329ee0ff188682ee7b8188b9a5dcdaba405744
Dennis H <111554091+frozzel@users.noreply.github.com>
2023-02-22 15:30:59 -0500

    import packages, OOP basics'

---
+++ day-16/main.py
@@ -0,0 +1,17 @@
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+
+# print(timmy)
+# timmy.shape("turtle") 
+# timmy.color("green")
+# timmy.resizemode("auto")
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+
+# timmy.forward(100)
+# my_screen.exitonclick()
+
+import prettytable
+

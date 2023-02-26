8005ab5969152fbb269abdb6d3e99962087aaab0
Dennis H <111554091+frozzel@users.noreply.github.com>
2023-02-25 19:47:32 -0500

    timmy spirograph

--- art_gallery/test.py
+++ art_gallery/test.py
@@ -1,13 +1,16 @@
 from turtle import Turtle, Screen
+import random
+import turtle as t
+t.colormode(255)
 
 timmy = Turtle()
-tommy = Turtle()
+# tommy = Turtle()
 
 timmy.shape("turtle")
 timmy.color("plum")
-tommy.shape("turtle")
-tommy.color("brown")
-tommy.shapesize(2, 2)
+# tommy.shape("turtle")
+# tommy.color("brown")
+# tommy.shapesize(2, 2)
 
 
 def draw_sq(turtle, dist, deg):
@@ -28,24 +31,7 @@ def draw_poop(turtle, dis):
 # draw_poop(timmy, 50)
 # draw_poop(tommy, 25)
 
-
-def six_shapes(turtle):
-    y = 3
-    x = 3
-    dis = 50
-    angle = 360 / x
-    while y < 11:
-        x +=1
-        dis +=50
-        y += 1
-        print(x, angle)
-        for _  in range(x):
-            turtle.fd(dis)
-            turtle.left(angle)
-      
-        
-
-# six_shapes(tommy)
+color = ["red", 'blue', 'green', 'purple', 'yellow', 'black', 'orange', ]
 
 
 def draw_shape(turtle, sides):
@@ -57,12 +43,44 @@ def draw_shape(turtle, sides):
         
 def turle_madness(turtle):
     for x in range(3,11):
+        turtle.color(random.choice(color))
         draw_shape(turtle, x)
 
-turle_madness(timmy)
-turle_madness(tommy)
+# turle_madness(timmy)
+# turle_madness(tommy)
 
+left_right = [90, 0, 180, 270]
 
+speed = [0,10,6,3,1]
+
+def rando_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    tuple = (r, g, b)
+    return tuple
+
+def rando_move(turtle):
+    turtle.width(10)
+    for _ in range(1, 50):
+        turtle.speed(random.choice(speed))
+        turtle.color(rando_color())
+        turtle.fd(50)
+        turtle.right( random.choice(left_right))
+
+# rando_move(timmy)
+# rando_move(tommy)
+
+def draw_spirograph( size_gap):
+    
+
+    timmy.speed("fastest")
+    for _  in range(int(360 / size_gap)):
+        timmy.color(rando_color())
+        timmy.circle(100)
+        timmy.setheading(timmy.heading() + size_gap)
+
+draw_spirograph(5)
 
 screen = Screen()
 screen.exitonclick()

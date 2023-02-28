dbbc52f3249ec71f518180efba4d9d41351a4fe6
Dennis H <111554091+frozzel@users.noreply.github.com>
2023-02-28 09:51:32 -0500

    turtle race finished

--- turtle_race/main.py
+++ turtle_race/main.py
@@ -6,25 +6,33 @@ screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet:", prompt="Which turtle will win the race: Enter Color:  ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 is_race_on = False
+y_pos = [-125, -75, -25, 25, 75, 125]
 turt_list = []
 
-def start_line(colors):
-    y= -125
-    for color in colors:
-        colors = Turtle(shape="turtle")
-        colors.color(color)
-        colors.penup()
-        colors.goto(x=-230, y=y)
-        y += 50
-        
 
+for index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_pos[index])
+    turt_list.append(new_turtle)
+        
+        
 if user_bet:
-    is_race_on =True
+    is_race_on = True
     
 while is_race_on:
-    
-    ran_dist = random.randint(0, 10)
-    colors.forward(ran_dist)
+     
+    for turtle in turt_list:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winner = turtle.pencolor()
+            if winner == user_bet:
+               print(f"You Won! The {winner} turtle is the winner!") 
+            else:
+                print(f"You've lost! The winner was {winner} turtle")
+        ran_dist = random.randint(0, 10)
+        turtle.forward(ran_dist)
 
-start_line(colors)
+
 screen.exitonclick()

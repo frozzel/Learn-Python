6a8749319ec4ab25ae1b58548f93ff5fa07b6cfc
frozzel <frozzel@frozzels-MacBook-Pro.local>
2023-03-24 14:55:57 -0400

    Stock Price Text through twillio

--- Stock Price/main.py
+++ Stock Price/main.py
@@ -1,4 +1,6 @@
 import requests
+from twilio.rest import Client
+
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -6,58 +8,59 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 STOCK_API = ""
+NEWS_API = ""
+account_sid = ""
+auth_token = ""
 
-params = {
+
+stock_params = {
     "function": "TIME_SERIES_DAILY_ADJUSTED",
     "symbol": STOCK_NAME,
     "interval": "60min",
     "apikey": STOCK_API
 }
 
-response = requests.get(STOCK_ENDPOINT, params=params)
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
 response.raise_for_status()
-data = response.json()
-
-print(data)
-
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
-
-#TODO 2. - Get the day before yesterday's closing stock price
-
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]['4. close']
+daybefore_yesterday_data = data_list[1]['4. close']
+difference = float(yesterday_data) - float(daybefore_yesterday_data)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+    
+dif_percent = round((difference / float(yesterday_data)) * 100)
 
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
+news_params = {
+    "qInTitle": COMPANY_NAME,
+    "apiKey": NEWS_API,
+    "pageSize": 3,
+    "page": 1,
+}
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+news_data = news_response.json()["articles"]
+three_articles = news_data[:3]
 
-#TODO 9. - Send each article as a separate message via Twilio. 
+formatted_art = [f"{STOCK_NAME}: {up_down}{dif_percent}% \nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
 
 
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+if abs(dif_percent) > .1:
+    for article in formatted_art:
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+                body=article,
+                from_='+18885951128',
+                to='your number'
+            )
+        print(message.status)
+        print(article)
+ 
+else:
+    print("No News")
 
